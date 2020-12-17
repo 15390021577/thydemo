54fe6c6f38afdf04de57ad910a97333b3cd6fe4c
offcn-java <2628166478@qq.com>
2020-12-17 17:35:39 +0800

    本人james添加了一个控制类

---
+++ src/main/java/com/offcn/thydemo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.offcn.thydemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/helloGit")
+    public String hell(){
+        System.out.println("----------");
+        return "success";
+    }
+}
